2a831ef1d3646565de00bd79d6e55716f6243e76
jwoo820 <48409306+jwoo820@users.noreply.github.com>
2022-01-18 21:01:26 +0900

    0118 c#

---
+++ Algorithm/B1011/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B1011");
+        }
+    }
+}
---
+++ 모의고사.js
@@ -0,0 +1,8 @@
+function solution(answers) {
+  var answer = [];
+  var one = [1, 2, 3, 4, 5];
+  var two = [2, 1, 2, 3, 2, 4, 2, 5];
+  var three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  return answer;
+}
