4b6fe4178051f8b1d7552cb513e997aec12518fd
jwoo820 <pjwoo820@gmail.com>
2022-09-24 08:11:12 +0900

    0924 프로그래머스 "문자열을 정수로 바꾸기"
    너무 쉬움

---
+++ StringToInt.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  StringToInt
+//  프로그래머스
+//  문자열을 정수로 바꾸기
+//  Created by Park Jungwoo on 2022/09/24.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
