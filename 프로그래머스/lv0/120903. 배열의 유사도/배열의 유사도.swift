b5fe70c6b145974e9a0a5e9a9a0d6b9f279e3581
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-03-13 11:21:32 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.swift
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.swift
@@ -1,15 +1,5 @@
 import Foundation
 
 func solution(_ s1:[String], _ s2:[String]) -> Int {
-    var answer = 0
-    for i in 0..<s1.count {
-        for j in 0..<s2.count {
-            if s1[i] == s2[j] {
-                answer += 1
-                break
-            }
-        }
-    }
-    
-    return answer
+    return s1.filter{s2.contains($0)}.count
 }
