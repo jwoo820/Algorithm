5ae08b25ebbf4c9dd42df308b5217d04cf4154a7
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-03-15 14:32:09 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
+    var coke = n
+    var count = 0
+    
+    while coke >= a {
+        count += coke/a*b
+        coke = coke/a*b + coke%a
+    }
+    return count
+}
