7ee3e6a5c5bd44b95c06f46e373f3714fbc01fdd
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-03-20 13:14:45 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    var str = s.replacingOccurrences(of: "one", with: "1")
+    .replacingOccurrences(of: "two", with: "2")
+    .replacingOccurrences(of: "three", with: "3")
+    .replacingOccurrences(of: "four", with: "4")
+    .replacingOccurrences(of: "five", with: "5")
+    .replacingOccurrences(of: "six", with: "6")
+    .replacingOccurrences(of: "seven", with: "7")
+    .replacingOccurrences(of: "eight", with: "8")
+    .replacingOccurrences(of: "nine", with: "9")
+    .replacingOccurrences(of: "zero", with: "0")
+    
+    return Int(str)!
+}
