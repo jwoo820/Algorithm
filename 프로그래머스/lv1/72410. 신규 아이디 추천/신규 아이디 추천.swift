f946654fd432157fa75695eeea4286091a3e8711
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-03-20 14:20:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.07 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func solution(_ new_id:String) -> String {
+    //1, 2
+    var id = new_id.lowercased()
+            .filter{$0 == "-" || $0 == "_" || $0 == "." || $0.isLetter || $0.isNumber}
+
+    // 3
+    while id.contains("..") {
+        id = id.replacingOccurrences(of: "..", with: ".")
+    }
+    // 4 
+    if id.first == "." {
+        id.removeFirst()
+    }
+    
+    if id.last == "." {
+        id.removeLast()
+    }
+    // 5 
+    if id.isEmpty {
+        id = "a"
+    }
+    // 6
+    if id.count > 15 {
+        id = String(id.prefix(15))
+        if id.last == "." {
+            id.removeLast()
+        }
+    }
+    // 7
+    if id.count < 3 {
+        while id.count != 3 {
+            id.append(id.last!)
+        }
+    }
+    return id
+}
