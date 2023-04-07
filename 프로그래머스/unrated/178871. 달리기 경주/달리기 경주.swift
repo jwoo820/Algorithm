f6168ccd3590da415525cc1a403a81b7ce7fb18a
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-04-07 15:15:31 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ players:[String], _ callings:[String]) -> [String] {
-    // 이름이 불리면 index 한칸 앞으로 이동
     
     var answer: [String] = []
     var players = players
@@ -13,8 +12,6 @@ func solution(_ players:[String], _ callings:[String]) -> [String] {
         playerDic[i] = player
     }
 
-    // print(playerDic)
-    // print(indexDic)
     for calling in callings {
         
         let callingIndex = indexDic[calling]! // 현재 불려진 선수의 index
@@ -25,17 +22,7 @@ func solution(_ players:[String], _ callings:[String]) -> [String] {
         playerDic[callingIndex] = frontPlayer // 원래 있던 선수를 뒤로 옮김
         indexDic[calling] = callingIndex-1
         indexDic[frontPlayer] = callingIndex
-//         playerDic[index] = calling
-//         indexDic[front] = index+1
-        
-//         indexDic[index] = calling
-        
-        
-        // let index = players.firstIndex(of: calling)!
-        // players.swapAt(index-1, index)
     }
-
-    // print(playerDic.sorted{ $0.key < $1.key}.map{$0.value})
-    // print(playerDic.sorted(by: {$0.key < $0.key}))
+    
     return playerDic.sorted{ $0.key < $1.key}.map{$0.value}
 }
