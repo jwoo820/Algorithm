8cff2ac1a8c15bcd6dd221e532d403f76121db7a
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-05-02 13:15:43 +0900

    [level 2] Title: 삼각 달팽이, Time: 29.18 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.swift
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.swift
@@ -2,29 +2,27 @@ import Foundation
 
 func solution(_ n:Int) -> [Int] {
     var answer: [[Int]] = []
-
-    for i in 1...n {
-        var arr = Array(repeating: 0, count: i)
-        answer.append(arr)
-    }
-    var index = 0
+    var index = 1
     var (x, y) = (-1, 0)
     
+    for i in 1...n {
+        answer.append(Array(repeating: 0, count: i))
+    }
+        
     for i in 0..<n {
         for j in i..<n {
-            if i%3 == 0 {
-                x += 1
-            } else if i % 3 == 1 {
+            if i%3 == 0 {        // 아래
+                x += 1      
+            } else if i%3 == 1 { // 오른쪽 이동
                 y += 1
-            } else {
+            } else {             // 위
                 x -= 1
                 y -= 1
             }
-            index += 1
             answer[x][y] = index
+            index += 1
         }
     }
     
-    // print(answer)
     return answer.flatMap{$0}
 }
