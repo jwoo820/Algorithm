cc1af276fdce5671902f893c556f883df3b948ee
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-05-05 11:37:00 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.23 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
@@ -1,32 +1,35 @@
 import Foundation
 
 func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    // 달 -> 날짜로 변경
-    
-    var termsDic: [String: Int] = [:]
-    for term in terms {
-        let separate = term.components(separatedBy: " ")
-        termsDic[separate[0]] = Int(separate[1])! * 28
-    }
-    
-    // today - privacies를 날짜로 계산
     var answer: [Int] = []
     
-    var todayArr = today.components(separatedBy: ".").map{Int($0)!}
-    for (index, privacy) in privacies.enumerated() {
-        let separate = privacy.components(separatedBy: " ")
-        // 개인정보 날짜
-        let privacyArr = separate[0].components(separatedBy: ".").map{Int($0)!}
-        let privacyType = separate[1]
-//         // 날짜 계산 해야함
-        let todayDate = todayArr[0]*12*28 + todayArr[1]*28 + todayArr[2]
-        let privacyDate = privacyArr[0]*12*28 + privacyArr[1]*28 + privacyArr[2] - 1
-        
-        let date = todayDate - privacyDate
-        if date > termsDic[privacyType]! {
-            answer.append(index+1)
+    var termsDays: [String: Int] = [:]
+    
+    for term in terms {
+        let split = term.components(separatedBy: " ")
+        termsDays[split[0]] = Int(split[1])! * 28
+    }
+    
+    let today = convertDay(today)
+    
+    for (i, privacy) in privacies.enumerated() {
+        var split = privacy.components(separatedBy: " ")
+        let term = split[1]
+        var privacyDay = convertDay(split[0])
+        if today - privacyDay >= termsDays[term]! {
+
+            answer.append(i+1)
         }
     }
     
     return answer
+}
+
+func convertDay(_ day: String) -> Int {
+    var split = day.components(separatedBy: ".")
+    var year = Int(split[0])! * 12 * 28
+    var month = Int(split[1])! * 28
+    var days = Int(split[2])!
+    
+    return year + month + days
 }
