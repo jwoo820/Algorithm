0bb0d6c9ab2b361a00a2cab759ad218a8b4c12f1
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-05-06 20:34:26 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.swift
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.swift
@@ -1,23 +1,27 @@
 import Foundation
 
 func solution(_ storey:Int) -> Int {
-    var answer: Int = Int.max
+    // 6~9 -> 10으로 올려서 계산
+    // 0~4 -> 0으로 내려서 계산
+    // 5 -> 다음 자리수가 5~9일 때는 10 else 0
+    var storey = storey
     
-    func dfs(storey: Int) {
-        var queue: [(Int, Int)] = [(storey, 0)]
-        
-        while !queue.isEmpty {
-            let (storey, count) = queue.removeLast()
-            if storey < 10 {
-                answer = min(answer, count + storey, count + 10 - storey + 1)
-                continue
+    var answer = 0
+    while storey != 0 {
+        var temp = storey % 10
+        if temp > 5 {
+            answer += 10 - temp
+            storey += 10
+        } else if temp < 5 {
+            answer += temp
+        } else {
+            if storey / 10 % 10 > 4 {
+                storey += 10
             }
-            
-            let n = storey % 10
-            queue.append((storey / 10, count+n))
-            queue.append((storey / 10 + 1, count+10-n))
+            answer += temp
         }
+        storey /= 10
     }
-    dfs(storey: storey)
+ 
     return answer
 }
