c2325a3f6dbbe8eacb57841b809d4ebc907a3ceb
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-05-18 13:41:10 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.88 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -1,26 +1,24 @@
 import Foundation
 
 func solution(_ wallpaper:[String]) -> [Int] {
-    var x: [Int] = []
-    var y: [Int] = []
+    var wallpaper = wallpaper.map{ $0.map{String($0)} }
+
+    var r_max = 0
+    var c_max = 0
     
-    for i in 0..<wallpaper.count {
-        let row = Array(wallpaper[i])
-        for j in 0..<row.count {
-            if row[j] == "#" {
-                y.append(i)
-                x.append(j)
+    var r_min = wallpaper.count
+    var c_min = wallpaper[0].count
+    
+    for (i, r) in wallpaper.enumerated() {
+        for (j, c) in r.enumerated() {
+            if c == "#" {
+                r_max = max(r_max, i)
+                c_max = max(c_max, j)
+                r_min = min(r_min, i)
+                c_min = min(c_min, j)
             }
         }
     }
     
-    // print(x)
-    // print(y)
-
-    var sortX = x.sorted()
-    var sortY = y.sorted()
-
-    // print(sortX)
-    // print(sortY)
-    return [sortY.first!, sortX.first!, sortY.last! + 1, sortX.last! + 1]
+    return [r_min, c_min, r_max+1, c_max+1]
 }
