9d52315fe3ae15e4a9681b1b97f85ac5a2b82305
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2023-07-13 21:52:24 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,6 +1,2 @@
 def solution(a, b):
-    answer = 0
-    ab = int(''.join(map(str,[a, b])))
-    ba = int(''.join(map(str,[b, a])))
-    
-    return ab if ab >= ba else ba
+    return int(max(f"{a}{b}", f"{b}{a}"))
